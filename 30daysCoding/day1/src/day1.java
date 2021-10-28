a61cdd493e6f5f1a1523f1a0186adcb3e69c0a9c
dankwx <63110149+dankwx@users.noreply.github.com>
2021-10-28 14:19:26 -0300

    Desafio 30 dias de coding do hackerrank.com
    https://www.hackerrank.com/danielkondlatsc1?hr_r=1

---
+++ 30daysCoding/day1/src/day1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String inputString;
+
+        inputString = scanner.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+
+    }
+
+}
